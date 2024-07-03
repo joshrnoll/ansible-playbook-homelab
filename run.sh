caefc9caa6abd096dcc300983bcc88dbb33be64a
Joshua <noll.joshua@outlook.com>
2024-07-03 16:58:53 -0400

    formatting and comment update

--- run.sh
+++ run.sh
@@ -2,13 +2,21 @@
 
 # Prompt for the become password
 read -sp 'BECOME password: ' BECOME_PASS
-echo  # Add a newline for better formatting
+
+# new line for formatting
+echo  
 
 # Prompt for the vault password
 read -sp 'VAULT password: ' VAULT_PASS
-echo  # Add a newline for better formatting
 
-# Run the ansible-playbook command with the passwords passed as extra vars
+# new line for formatting
+echo  
+
+# Set password variables
 ANSIBLE_BECOME_PASS="$BECOME_PASS" ANSIBLE_VAULT_PASS="$VAULT_PASS" \
+
+# Call playbook for VM creation
 ansible-playbook -i hosts.ini baseline/proxmox/main.yml --extra-vars "ansible_become_pass=$BECOME_PASS ansible_vault_password=$VAULT_PASS"
+
+# Call main.yml for homelab configuration
 ansible-playbook -i hosts.ini main.yml --extra-vars "ansible_become_pass=$BECOME_PASS ansible_vault_password=$VAULT_PASS"
