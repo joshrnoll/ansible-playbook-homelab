d7934157b02033ef88a8373d69f6d78a18385d5d
Joshua <noll.joshua@outlook.com>
2024-10-10 07:05:06 -0400

    moved proxmox configuration to its own folder

--- run.sh
+++ run.sh
@@ -15,8 +15,8 @@ echo
 # Set password variables
 ANSIBLE_BECOME_PASS="$BECOME_PASS" ANSIBLE_VAULT_PASS="$VAULT_PASS" \
 
-# Call playbook for VM creation
-ansible-playbook -i hosts.ini baseline/proxmox/main.yml --extra-vars "ansible_become_pass=$BECOME_PASS ansible_vault_password=$VAULT_PASS"
+# Call playbook for Proxmox setup and VM creation
+ansible-playbook -i production.yml baseline/proxmox/main.yml --extra-vars "ansible_become_pass=$BECOME_PASS ansible_vault_password=$VAULT_PASS"
 
 # Call main.yml for homelab configuration
-ansible-playbook -i hosts.ini main.yml --extra-vars "ansible_become_pass=$BECOME_PASS ansible_vault_password=$VAULT_PASS"
+ansible-playbook -i production.yml main.yml --extra-vars "ansible_become_pass=$BECOME_PASS ansible_vault_password=$VAULT_PASS"
